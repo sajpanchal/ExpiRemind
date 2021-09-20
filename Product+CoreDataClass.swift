e1921a98667de8432fd584e2178eee06280aacc6
saj panchal <sajpanchal2020@outlook.com>
2021-09-20 10:34:52 -0400

    added core data entity called product.

---
+++ Product+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Product+CoreDataClass.swift
+//  NotifyExpiryDate (iOS)
+//
+//  Created by saj panchal on 2021-09-20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Product)
+public class Product: NSManagedObject {
+
+}
--- Shared/Persistence.swift
+++ Shared/Persistence.swift
@@ -14,11 +14,11 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+           // let newItem = Item(context: viewContext)
+           // newItem.timestamp = Date()
         }
         do {
-            try viewContext.save()
+           // try viewContext.save()
         } catch {
             // Replace this implementation with code to handle the error appropriately.
             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
